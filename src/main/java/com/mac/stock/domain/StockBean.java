be86130c1a4ee96cd01e27ac1dcb8fe7e0bbf127
马超 <machaop@dcits.com>
2020-03-05 17:10:40 +0800

    基础实体类

---
+++ src/main/java/com/mac/stock/domain/StockBean.java
@@ -0,0 +1,328 @@
+package com.mac.stock.domain;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name = "stock")
+public class StockBean {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long stockid;
+    @Column
+    private String stocknum;
+    @Column
+    private String stockname;
+    /**
+     * 表名，查询用，非实体属性
+     */
+    private String stockTableName;
+    //公司代码，解析json用
+    private String COMPANY_CODE;
+    //公司简称，解析json用
+    private String COMPANY_ABBR;
+    //上市日期
+    @Column
+    private String LISTING_DATE;
+
+    //流通股本
+    @Column
+    private double totalFlowShares;
+    //总股本
+    @Column
+    private double totalShares;
+
+    /**
+     * 涨跌幅
+     */
+    @Column(precision = 5, scale = 4)
+    private double updownrange;
+
+    @Column(precision = 5, scale = 4)
+    private double updownrange3;
+
+    @Column(precision = 5, scale = 4)
+    private double updownrange5;
+
+    /**
+     * 换手率
+     */
+    @Column(precision = 5, scale = 4)
+    private double turnoverrate;
+
+    /**
+     * 涨跌额
+     */
+    @Column(precision = 10, scale = 3)
+    private double updownprices;
+
+    @Column(precision = 10, scale = 3)
+    private double open;
+
+    @Column(precision = 10, scale = 3)
+    private double close;
+
+    @Column(precision = 10, scale = 3)
+    private double high;
+
+    @Column(precision = 10, scale = 3)
+    private double low;
+
+    @Column(precision = 10, scale = 3)
+    private double preclose;
+
+    @Column(precision = 15)
+    private double volume;
+
+    @Column(precision = 15)
+    private double amount;
+
+    /**
+     * 振幅
+     */
+    @Column(precision = 5, scale = 4)
+    private double amplitude;
+
+    @Column(precision = 15)
+    private double totalmarketvalue;
+
+    @Column(precision = 15)
+    private double flowmarketvalue;
+
+    private Date updateDate;
+
+
+    public StockBean() {
+    }
+
+    public StockBean(int stockid) {
+        this.stockid = stockid;
+    }
+
+    public long getStockid() {
+        return stockid;
+    }
+
+    public void setStockid(long stockid) {
+        this.stockid = stockid;
+    }
+
+    public String getStocknum() {
+        return stocknum;
+    }
+
+    public void setStocknum(String stocknum) {
+        this.stocknum = stocknum;
+    }
+
+    public String getStockname() {
+        return stockname;
+    }
+
+    public void setStockname(String stockname) {
+        this.stockname = stockname;
+    }
+
+    public String getStockTableName() {
+        return stockTableName;
+    }
+
+    public void setStockTableName(String stockTableName) {
+        this.stockTableName = stockTableName;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        // 两个对象是同一个时，直接return true
+        if (this == obj) {
+            return true;
+        }
+        if (obj instanceof StockBean) {
+            // 比较对象也是StockBean对象时，判断name和age是否都相同
+            StockBean p = (StockBean) obj;
+            return stocknum.equals(p.stocknum) && stockname.equals(p.stockname);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    public String getCOMPANY_ABBR() {
+        return COMPANY_ABBR;
+    }
+
+    public void setCOMPANY_ABBR(String cOMPANY_ABBR) {
+        COMPANY_ABBR = cOMPANY_ABBR;
+    }
+
+    public String getCOMPANY_CODE() {
+        return COMPANY_CODE;
+    }
+
+    public void setCOMPANY_CODE(String cOMPANY_CODE) {
+        COMPANY_CODE = cOMPANY_CODE;
+    }
+
+    public String getLISTING_DATE() {
+        return LISTING_DATE;
+    }
+
+    public void setLISTING_DATE(String lISTING_DATE) {
+        LISTING_DATE = lISTING_DATE;
+    }
+
+    public double getTotalFlowShares() {
+        return totalFlowShares;
+    }
+
+    public void setTotalFlowShares(double totalFlowShares) {
+        this.totalFlowShares = totalFlowShares;
+    }
+
+    public double getTotalShares() {
+        return totalShares;
+    }
+
+    public void setTotalShares(double totalShares) {
+        this.totalShares = totalShares;
+    }
+
+    public double getUpdownrange() {
+        return updownrange;
+    }
+
+    public void setUpdownrange(double updownrange) {
+        this.updownrange = updownrange;
+    }
+
+    public double getUpdownrange3() {
+        return updownrange3;
+    }
+
+    public void setUpdownrange3(double updownrange3) {
+        this.updownrange3 = updownrange3;
+    }
+
+    public double getUpdownrange5() {
+        return updownrange5;
+    }
+
+    public void setUpdownrange5(double updownrange5) {
+        this.updownrange5 = updownrange5;
+    }
+
+    public double getTurnoverrate() {
+        return turnoverrate;
+    }
+
+    public void setTurnoverrate(double turnoverrate) {
+        this.turnoverrate = turnoverrate;
+    }
+
+    public double getUpdownprices() {
+        return updownprices;
+    }
+
+    public void setUpdownprices(double updownprices) {
+        this.updownprices = updownprices;
+    }
+
+    public double getOpen() {
+        return open;
+    }
+
+    public void setOpen(double open) {
+        this.open = open;
+    }
+
+    public double getClose() {
+        return close;
+    }
+
+    public void setClose(double close) {
+        this.close = close;
+    }
+
+    public double getHigh() {
+        return high;
+    }
+
+    public void setHigh(double high) {
+        this.high = high;
+    }
+
+    public double getLow() {
+        return low;
+    }
+
+    public void setLow(double low) {
+        this.low = low;
+    }
+
+    public double getPreclose() {
+        return preclose;
+    }
+
+    public void setPreclose(double preclose) {
+        this.preclose = preclose;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public void setVolume(double volume) {
+        this.volume = volume;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public double getAmplitude() {
+        return amplitude;
+    }
+
+    public void setAmplitude(double amplitude) {
+        this.amplitude = amplitude;
+    }
+
+    public double getTotalmarketvalue() {
+        return totalmarketvalue;
+    }
+
+    public void setTotalmarketvalue(double totalmarketvalue) {
+        this.totalmarketvalue = totalmarketvalue;
+    }
+
+    public double getFlowmarketvalue() {
+        return flowmarketvalue;
+    }
+
+    public void setFlowmarketvalue(double flowmarketvalue) {
+        this.flowmarketvalue = flowmarketvalue;
+    }
+
+    public Date getUpdateDate() {
+        if (updateDate == null) {
+            return null;
+        }
+        return (Date) updateDate.clone();
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        if (updateDate == null) {
+            this.updateDate = null;
+        } else {
+            this.updateDate = (Date) updateDate.clone();
+        }
+
+    }
+}
---
+++ src/main/java/com/mac/stock/domain/StockMonthInfo.java
@@ -0,0 +1,368 @@
+package com.mac.stock.domain;
+
+import java.sql.Date;
+
+public class StockMonthInfo {
+    private String monthinfoid;
+
+    private int stockid;
+
+    /**
+     * 股票代码
+     */
+    private String stockcode;
+
+    /**
+     * 日期
+     */
+    private Date date;
+    /**
+     * 今开
+     */
+    private double open;
+    /**
+     * 最高
+     */
+    private double high;
+    /**
+     * 最低
+     */
+    private double low;
+    /**
+     * 今收
+     */
+    private double close;
+    /**
+     * 成交量
+     */
+    private double volume;
+    /**
+     * 成交额
+     */
+    private double amount;
+    private double ccl;
+    /**
+     * 昨收
+     */
+    private double preClose;
+    /**
+     * 涨幅
+     */
+    private double netChangeRatio;
+    //指标
+    private double k;
+    private double d;
+    private double j;
+    private double kline;
+    private double ma5;
+    private double ma10;
+    private double ma20;
+    private double ma30;
+    private double ma60;
+    private double ma120;
+    private double ma200;
+    private double ma250;
+    private double dif;
+    private double dea;
+    private double macd;
+    private double rsi6;
+    private double rsi12;
+    private double rsi24;
+    private double wr10;
+    private double wr6;
+
+
+    public StockMonthInfo() {
+        super();
+    }
+
+    public StockMonthInfo(String stockcode, Date date, double open, double high, double low, double close,
+                          double volume, double amount) {
+        super();
+        this.stockcode = stockcode;
+        if (date == null) {
+            this.date = null;
+        } else {
+            this.date = (Date) date.clone();
+        }
+
+        this.open = open;
+        this.high = high;
+        this.low = low;
+        this.close = close;
+        this.volume = volume;
+        this.amount = amount;
+    }
+
+    public String getMonthinfoid() {
+        return monthinfoid;
+    }
+
+    public void setMonthinfoid(String monthinfoid) {
+        this.monthinfoid = monthinfoid;
+    }
+
+    public int getStockid() {
+        return stockid;
+    }
+
+    public void setStockid(int stockid) {
+        this.stockid = stockid;
+    }
+
+    public String getStockcode() {
+        return stockcode;
+    }
+
+    public void setStockcode(String stockcode) {
+        this.stockcode = stockcode;
+    }
+
+    public Date getDate() {
+        if (date == null) {
+            return null;
+        }
+        return (Date) date.clone();
+    }
+
+    public void setDate(Date date) {
+        if (date == null) {
+            this.date = null;
+        } else {
+            this.date = (Date) date.clone();
+        }
+
+    }
+
+    public double getOpen() {
+        return open;
+    }
+
+    public void setOpen(double open) {
+        this.open = open;
+    }
+
+    public double getHigh() {
+        return high;
+    }
+
+    public void setHigh(double high) {
+        this.high = high;
+    }
+
+    public double getLow() {
+        return low;
+    }
+
+    public void setLow(double low) {
+        this.low = low;
+    }
+
+    public double getClose() {
+        return close;
+    }
+
+    public void setClose(double close) {
+        this.close = close;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public void setVolume(double volume) {
+        this.volume = volume;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public double getCcl() {
+        return ccl;
+    }
+
+    public void setCcl(double ccl) {
+        this.ccl = ccl;
+    }
+
+    public double getPreClose() {
+        return preClose;
+    }
+
+    public void setPreClose(double preClose) {
+        this.preClose = preClose;
+    }
+
+    public double getNetChangeRatio() {
+        return netChangeRatio;
+    }
+
+    public void setNetChangeRatio(double netChangeRatio) {
+        this.netChangeRatio = netChangeRatio;
+    }
+
+    public double getK() {
+        return k;
+    }
+
+    public void setK(double k) {
+        this.k = k;
+    }
+
+    public double getD() {
+        return d;
+    }
+
+    public void setD(double d) {
+        this.d = d;
+    }
+
+    public double getJ() {
+        return j;
+    }
+
+    public void setJ(double j) {
+        this.j = j;
+    }
+
+    public double getKline() {
+        return kline;
+    }
+
+    public void setKline(double kline) {
+        this.kline = kline;
+    }
+
+    public double getMa5() {
+        return ma5;
+    }
+
+    public void setMa5(double ma5) {
+        this.ma5 = ma5;
+    }
+
+    public double getMa10() {
+        return ma10;
+    }
+
+    public void setMa10(double ma10) {
+        this.ma10 = ma10;
+    }
+
+    public double getMa20() {
+        return ma20;
+    }
+
+    public void setMa20(double ma20) {
+        this.ma20 = ma20;
+    }
+
+    public double getMa30() {
+        return ma30;
+    }
+
+    public void setMa30(double ma30) {
+        this.ma30 = ma30;
+    }
+
+    public double getMa60() {
+        return ma60;
+    }
+
+    public void setMa60(double ma60) {
+        this.ma60 = ma60;
+    }
+
+    public double getMa120() {
+        return ma120;
+    }
+
+    public void setMa120(double ma120) {
+        this.ma120 = ma120;
+    }
+
+    public double getMa200() {
+        return ma200;
+    }
+
+    public void setMa200(double ma200) {
+        this.ma200 = ma200;
+    }
+
+    public double getMa250() {
+        return ma250;
+    }
+
+    public void setMa250(double ma250) {
+        this.ma250 = ma250;
+    }
+
+    public double getDif() {
+        return dif;
+    }
+
+    public void setDif(double dif) {
+        this.dif = dif;
+    }
+
+    public double getDea() {
+        return dea;
+    }
+
+    public void setDea(double dea) {
+        this.dea = dea;
+    }
+
+    public double getMacd() {
+        return macd;
+    }
+
+    public void setMacd(double macd) {
+        this.macd = macd;
+    }
+
+    public double getRsi6() {
+        return rsi6;
+    }
+
+    public void setRsi6(double rsi6) {
+        this.rsi6 = rsi6;
+    }
+
+    public double getRsi12() {
+        return rsi12;
+    }
+
+    public void setRsi12(double rsi12) {
+        this.rsi12 = rsi12;
+    }
+
+    public double getRsi24() {
+        return rsi24;
+    }
+
+    public void setRsi24(double rsi24) {
+        this.rsi24 = rsi24;
+    }
+
+    public double getWr10() {
+        return wr10;
+    }
+
+    public void setWr10(double wr10) {
+        this.wr10 = wr10;
+    }
+
+    public double getWr6() {
+        return wr6;
+    }
+
+    public void setWr6(double wr6) {
+        this.wr6 = wr6;
+    }
+}
