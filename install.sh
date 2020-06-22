6fad8b9f9fa13dae42ca8e2067dae933a897a58d
马超 <machaop@dcits.com>
2020-06-22 14:19:53 +0800

    自动化部署测试

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+mvn install -s stock-common/src/main/resources/conf/settings.xml
+
---
+++ stock-web/src/main/resources/script/startup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+BUILD_ID=dontKillMe
+
+nohup java -jar app/stock-web/stock-web-1.0.0-SNAPSHOT.jar --server.port=8764 &
+
+
+
